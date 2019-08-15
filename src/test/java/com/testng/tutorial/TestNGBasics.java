d26c30b5e0b9ca437bfa5a8952198404d3d1430b
Andrea Johnson <andrea.johnson@vml.com>
2019-08-15 16:35:17 -0500

    First testng annotation

--- src/test/java/com/testng/tutorial/TestNGBasics.java
+++ src/test/java/com/testng/tutorial/TestNGBasics.java
@@ -1,8 +1,11 @@
 package com.testng.tutorial;
 
+import org.testng.annotations.Test;
+
 public class TestNGBasics {
 
-    public static void main(String[] args) {
-
+    @Test
+    public void Demo() {
+        System.out.println("Heya");
     }
 }
